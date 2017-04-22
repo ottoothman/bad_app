93273b1d90fb1c2c189f3d6230c7f86e9d3c5c28
AchillesTurtle <achillesturtle@pobox.com>
2017-04-22 15:16:49 -0400

    Seeded users and modified decision_group

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :decisions
-  devise_scope :users do
-    get 'login', to: 'devise/sessions#new'
-  end
+  root to: 'decisions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+10.times do |i|
+  User.create(email: "#{i}dummy@aol.com", password: "123456")
+  puts "NEW USER CREATED!!!"
+end
